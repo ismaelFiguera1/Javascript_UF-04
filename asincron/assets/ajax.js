07cc0cdb3950ccd8edb0a2c92cd160202f1380f3
ismaelFiguera <ismaelfiguera112@gnail.com>
2025-02-12 19:05:54 +0100

    api llegir

--- asincron/assets/ajax.js
+++ asincron/assets/ajax.js
@@ -1,14 +1,28 @@
 (() => {
   // primer pas
   const xhr = new XMLHttpRequest(),
-    $xhr = document.querySelector("#xhr");
-  console.log(xhr);
+    $xhr = document.querySelector("#xhr"),
+    $fragment = document.createDocumentFragment();
+  //console.log(xhr);
   // segon pas
   xhr.addEventListener("readystatechange", (e) => {
-    console.log(e);
+    if (xhr.readyState != 4) return;
+    // Nomes gestionarem l'estat 4 "Complete"
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let json = JSON.parse(xhr.responseText);
+      json.forEach((e) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${e.id} -- ${e.name} -- ${e.username} -- ${e.email}`;
+        $fragment.appendChild($li);
+      });
+      $xhr.appendChild($fragment);
+    } else {
+      let message = xhr.statusText || "S'ha produit un error";
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+    }
   });
   // tercer pas
-  xhr.open("GET", "https://api.github.com/users/ismaelFiguera1");
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/use");
   // quart pas
   xhr.send();
 })(); /* Aixo en una funcio autoexecutable */
