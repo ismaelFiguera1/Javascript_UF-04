a03a502be61241eee7d9b1c4c0ef014188c7b416
ismaelFiguera <ismaelfiguera112@gnail.com>
2025-04-01 19:08:03 +0200

    state

---
+++ REACTIVITAT/assets/script.js
@@ -0,0 +1,76 @@
+let d = document,
+  $item = d.querySelector("#todo-item"),
+  $list = d.getElementById("todo-list");
+
+//Estat
+const state = {
+  todoList: [],
+};
+
+const getState = () => JSON.parse(JSON.stringify(template.data));
+/*
+const items = getState();
+items.todoList.push("Tasca 6");
+
+console.log(state);
+console.log(items);
+*/
+d.addEventListener("submit", (e) => {
+  if (!e.target.matches("#todo-form")) return false;
+  e.preventDefault();
+  if (!$item.value) return;
+  // Actualitzar l'estat de forma reactiva
+  const lastState = getState();
+  lastState.todoList.push($item.value);
+  // state.todoList.push($item.value);
+  setState({
+    todoList: lastState.todoList,
+  });
+  //render();
+  /*
+  let $i = d.createElement("li");
+  $i.textContent = $item.value;
+  $list.appendChild($i);
+    */
+  $item.value = "";
+  $item.focus();
+});
+
+//actualitzar l'estat de forma reactiva
+
+//TemplateUI
+
+const template = () => {
+  if (state.todoList.length < 1) return `<p><em>Llista sense tasques</em></p>`;
+  let todos = template.data.todoList.map((item) => `<li>${item}</li>`).join("");
+  return todos;
+};
+
+//letat leocal dels components
+template.data = {
+  todoList: [],
+};
+
+//Render
+const render = () => {
+  console.log(template.data);
+  const $list = d.getElementById("todo-list");
+  if (!$list) return;
+  $list.innerHTML = template();
+};
+
+const setState = (obj) => {
+  for (let key in obj) {
+    if (template.data.hasOwnProperty(key)) {
+      template.data[key] = obj[key];
+    }
+  }
+  render();
+};
+
+setState({
+  nom: "toni",
+  todoList: ["Tasca 1", "Tasca 2", "Tasca 3", "Tasca 4", "Tasca 5"],
+});
+
+d.addEventListener("DOMContentLoaded", render);
